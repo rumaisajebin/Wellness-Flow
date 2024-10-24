5e7be3fec98a52f492cbc92bb10ea4e9615f4956
rumaisajebin <jebinrumaisa@gmail.com>
2024-10-24 19:48:24 +0530

    clearing bugs in email otp

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -224,12 +224,18 @@ BASE_URL = "http://127.0.0.1:8000/"
 load_dotenv()
 
 # Email settings
-EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))  # Provide a default value
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER='jebinrumaisa@gmail.com'
+EMAIL_HOST_PASSWORD='csqo yior dtey iffx'
+# EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+# EMAIL_HOST = os.getenv('EMAIL_HOST')
+# EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
+# EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))  # Provide a default value
+# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 # Stripe settings
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')

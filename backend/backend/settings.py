ed8bcf507758ac9a4c52d7663b7533bbccfc72fc
Moahmed-Gouse <gouasgauz@gmail.com>
2024-11-02 15:07:10 +0530

    Image problem solved

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -175,9 +175,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-BASE_URL = "https://rareblu.shop/"
-MEDIA_URL = BASE_URL + 'media/'
-# MEDIA_URL = '/media/'
+# BASE_URL = "https://rareblu.shop/"
+# MEDIA_URL = BASE_URL + 'media/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
